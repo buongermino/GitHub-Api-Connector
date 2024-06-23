e8b25ffe5d38d72052d1c2b8a54f4ebdd611d69c
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-23 16:33:32 -0300

    added controller and some configs to Program.cs. Deleted .http unused file

---
+++ GitHubApiConnector/Controllers/GitHubConnectorController.cs
@@ -0,0 +1,29 @@
+﻿using GitHubApiConnector.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubApiConnector.Api.Controllers;
+
+[Route("api/[controller]/repositories")]
+[ApiController]
+public class GitHubConnectorController : ControllerBase
+{
+    [HttpGet("fetch-and-save")]
+    public async Task<IActionResult> FetchAndSaveRepositories([FromServices] IFetchAndSaveRepositoriesUseCase fetchAndSaveRepositoriesUseCase)
+    {
+        var respositories = await fetchAndSaveRepositoriesUseCase.Execute();
+
+        return Ok(respositories);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> ListRepositories()
+    {
+        return NoContent();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> ListRepositoryById()
+    {
+      return Ok();
+    }
+}
--- GitHubApiConnector/Program.cs
+++ GitHubApiConnector/Program.cs
@@ -1,11 +1,26 @@
+using GitHubApiConnector.Infrastructure;
+using GitHubApiConnector.Infrastructure.Interfaces;
+using GitHubApiConnector.UseCases;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddHttpClient<IGitHubApiClient, GitHubApiClient>();
+builder.Services.AddScoped<IFetchAndSaveRepositoriesUseCase, FetchAndSaveRepositoriesUseCase>();
 
 var app = builder.Build();
 
