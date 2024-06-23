ee7b7d7acb1807bfe57761a19a8ae4069fc0c148
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-23 16:32:20 -0300

    added UseCase layer and fetch-and-save useCase initial interface and its implementation

---
+++ GitHubApiConnector.UseCases/FetchAndSaveRepositoriesUseCase.cs
@@ -0,0 +1,21 @@
+﻿using GitHubApiConnector.Infrastructure;
+using GitHubApiConnector.Infrastructure.Interfaces;
+
+namespace GitHubApiConnector.UseCases;
+
+public interface IFetchAndSaveRepositoriesUseCase
+{
+    Task<GitHubRepositoryResponse> Execute();
+}
+
+public class FetchAndSaveRepositoriesUseCase(IGitHubApiClient gitHubApiClient) : IFetchAndSaveRepositoriesUseCase
+{
+    public async Task<GitHubRepositoryResponse> Execute()
+    {
+        var languages = new List<string>() { "csharp", "java", "angular", "golang", "kotlin" };
+
+        var repos = await gitHubApiClient.FetchRepositories(languages);
+
+        return repos;
+    }
+}
