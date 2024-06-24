f6a37ac2035c8d0be6859f48c9d21d02a8fced68
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-24 20:28:44 -0300

    updated db context

--- GitHubApiConnector.Infrastructure/Data/GitHubRepoContext.cs
+++ GitHubApiConnector.Infrastructure/Data/GitHubRepoContext.cs
@@ -2,29 +2,17 @@
 using GitHubApiConnector.Domain.SeedWork;
 using GitHubApiConnector.Infrastructure.Data.EntityConfiguration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 
 namespace GitHubApiConnector.Infrastructure.Data;
 
 public class GitHubRepoContext : DbContext, IUnitOfWork
 {
-    public GitHubRepoContext()
-    {
-    }
+    public GitHubRepoContext() { }
 
     public GitHubRepoContext(DbContextOptions<GitHubRepoContext> options) : base(options) { }
 
     public DbSet<GitHubRepo> GitHubRepos { get; set; }
     public DbSet<GitHubRepoOwner> GitHubRepoOwners { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new GitHubRepoEntityTypeConfiguration());
-        modelBuilder.ApplyConfiguration(new GitHubRepoOwnerEntityTypeConfiguration());
-
-        base.OnModelCreating(modelBuilder);
-    }
 
     public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
     {
@@ -32,23 +20,19 @@ public class GitHubRepoContext : DbContext, IUnitOfWork
 
         return true;
     }
-}
 
-public class BankingContextDesignFactory : IDesignTimeDbContextFactory<GitHubRepoContext>
-{
-    public GitHubRepoContext CreateDbContext(string[] args)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../GitHubApiConnector.Api");
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(basePath)
-            .AddJsonFile("appsettings.json")
-            .Build();
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=NBQFC-1M38ST3;Database=github_repos_db;User ID=sa;Password=Fcamara@2024;Trust Server Certificate=True");
+        }
+    }
 
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        var optionsBuilder = new DbContextOptionsBuilder<GitHubRepoContext>()
-            .UseSqlServer(connectionString);
-
-        return new GitHubRepoContext(optionsBuilder.Options);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new GitHubRepoEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new GitHubRepoOwnerEntityTypeConfiguration());
+        base.OnModelCreating(modelBuilder);
     }
 }
