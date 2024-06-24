3c5781e8392ef5d67de69ae6183ba3f83b2a895c
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-24 19:28:51 -0300

    added entity type configuration

---
+++ GitHubApiConnector.Infrastructure/Data/EntityConfiguration/GitHubRepoEntityTypeConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using GitHubApiConnector.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GitHubApiConnector.Infrastructure.Data.EntityConfiguration;
+
+public class GitHubRepoEntityTypeConfiguration : IEntityTypeConfiguration<GitHubRepo>
+{
+    public void Configure(EntityTypeBuilder<GitHubRepo> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.GitHubId);
+
+        builder.Property(p => p.Name);
+
+        builder.Property(p => p.FullName);
+
+        builder.Property(p => p.Description);
+
+        builder.Property(p => p.Url);
+
+        builder.Property(p => p.GitUrl);
+
+        builder.Property(p => p.SshUrl);
+
+        builder.Property(p => p.HtmlUrl);
+
+        builder.Property(p => p.CloneUrl);
+
+        builder.Property(p => p.Forks);
+
+        builder.Property(p => p.Language);
+
+        builder.Property(p => p.CreatedAt);
+
+        builder.Property(p => p.UpdatedAt);
+
+        builder.Property(p => p.PushedAt);
+
+        builder.Property(p => p.GitHubRepoOwnerId);
+
+        builder.HasOne(p => p.Owner)
+            .WithMany(p => p.Repos)
+            .HasForeignKey(p => p.GitHubRepoOwnerId);
+    }
+}
---
+++ GitHubApiConnector.Infrastructure/Data/EntityConfiguration/GitHubRepoOwnerEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GitHubApiConnector.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GitHubApiConnector.Infrastructure.Data.EntityConfiguration;
+
+public class GitHubRepoOwnerEntityTypeConfiguration : IEntityTypeConfiguration<GitHubRepoOwner>
+{
+    public void Configure(EntityTypeBuilder<GitHubRepoOwner> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.GitHubUserId);
+
+        builder.Property(p => p.Login);
+
+        builder.Property(p => p.Url); 
+        
+        builder.Property(p => p.AvatarUrl);
+
+        builder.Property(p => p.ReposUrl);
+
+        builder.Property(p => p.HtmlUrl);
+
+        builder.Property(p => p.Type);
+
+        builder.Property(p => p.OrganizationsUrl);
+
+        builder.HasMany(p => p.Repos)
+            .WithOne(p => p.Owner)
+            .HasForeignKey(p => p.GitHubRepoOwnerId);
+    }
+}
