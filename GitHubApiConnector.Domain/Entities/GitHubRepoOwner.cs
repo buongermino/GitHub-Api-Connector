2fa8c69511826b5fdfa62836dee2538e6ddec887
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-24 19:28:17 -0300

    added GithubRepo and Owner entities

---
+++ GitHubApiConnector.Domain/Entities/GitHubRepoOwner.cs
@@ -0,0 +1,19 @@
+﻿using GitHubApiConnector.Domain.SeedWork;
+
+namespace GitHubApiConnector.Domain.Entities;
+
+public class GitHubRepoOwner : Entity
+{
+    public GitHubRepoOwner() {}
+
+    public string Login { get; set; }
+    public string GitHubUserId { get; set; }
+    public string Url { get; set; }
+    public string AvatarUrl { get; set; }
+    public string HtmlUrl { get; set; }
+    public string ReposUrl { get; set; }
+    public string OrganizationsUrl { get; set; }
+    public string Type { get; set; }
+
+    public List<GitHubRepo> Repos { get; set; }
+}
