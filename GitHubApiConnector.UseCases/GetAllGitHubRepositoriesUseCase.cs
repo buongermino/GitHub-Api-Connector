42855d17ab030d33e6d5167b831ebe476a2a6cfd
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-25 19:42:51 -0300

    added GetAllGitHubRepositoriesUseCase

---
+++ GitHubApiConnector.UseCases/GetAllGitHubRepositoriesUseCase.cs
@@ -0,0 +1,37 @@
+﻿using GitHubApiConnector.Domain.Entities;
+using GitHubApiConnector.Domain.IRepository;
+
+namespace GitHubApiConnector.UseCases;
+
+public interface IGetAllGitHubRepositoriesUseCase 
+{
+    Task<List<GitHubRepo>> Execute(string filterByLanguage, int pageNumber, int pageSize);
+}
+
+
+public class GetAllGitHubRepositoriesUseCase(IGitHubRepoRepository repository) : IGetAllGitHubRepositoriesUseCase
+{
+    public async Task<List<GitHubRepo>> Execute(string filterByLanguage, int pageNumber, int pageSize)
+    {
+        if (pageNumber <= 0) pageNumber = 1;
+        if (pageSize <= 0) pageSize = 10;
+
+        int skip = (pageNumber - 1) * pageSize;
+
+        var repositories = await repository.GetAllAsync();
+
+        if (!string.IsNullOrEmpty(filterByLanguage))
+        {
+            if (filterByLanguage == "csharp") filterByLanguage = "C#";
+            repositories = repositories.Where(repo => repo.Language.Equals(filterByLanguage, StringComparison.CurrentCultureIgnoreCase)).ToList();
+        }
+
+        List<GitHubRepo> paginatedRepos = repositories
+                                           .Skip(skip)
+                                           .Take(pageSize)
+                                           .ToList();
+
+        return paginatedRepos;
+    }
+}
+
