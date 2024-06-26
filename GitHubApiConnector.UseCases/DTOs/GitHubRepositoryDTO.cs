77125e9cb34051203629820a8bf890bf16efc362
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-26 15:58:33 -0300

    added DTOs

---
+++ GitHubApiConnector.UseCases/DTOs/GitHubRepositoryDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace GitHubApiConnector.UseCases.DTOs;
+
+public class GitHubRepositoryDTO
+{
+    public Guid Id { get; set; }
+    public string Language { get; set; }
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public int Watchers { get; set; }
+    public int Forks { get; set; }
+    public string GitUrl { get; set; }
+    public string CloneUrl { get; set; }
+}
---
+++ GitHubApiConnector.UseCases/DTOs/GitHubRepositoryDetailsDTO.cs
@@ -0,0 +1,25 @@
+﻿using GitHubApiConnector.Domain.Entities;
+
+namespace GitHubApiConnector.UseCases.DTOs;
+
+public class GitHubRepositoryDetailsDTO
+{
+    public Guid Id { get; set; }
+    public string GitHubId { get; set; }
+    public string Language { get; set; }
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public string GitUrl { get; set; }
+    public string SshUrl { get; set; }
+    public string CloneUrl { get; set; }
+    public int Size { get; set; }
+    public int Watchers { get; set; }
+    public int Forks { get; set; }
+    public string HtmlUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime PushedAt { get; set; }
+    public GitHubRepoOwner Owner { get; set; }
+}
