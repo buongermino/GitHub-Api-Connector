50b7e9b1e8c9b5260dad5b2e9b9e9973ac776c63
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-26 20:48:37 -0300

    set up automatic migration on start application

--- GitHubApiConnector/Program.cs
+++ GitHubApiConnector/Program.cs
@@ -1,6 +1,8 @@
 using GitHubApiConnector.Infrastructure;
+using GitHubApiConnector.Infrastructure.Data;
 using GitHubApiConnector.Infrastructure.Interfaces;
 using GitHubApiConnector.UseCases;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
@@ -52,6 +54,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+//Execute migrations when application starts
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<GitHubRepoContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while applying the migrations.");
+    }
 }
 
 app.UseHttpsRedirection();
