0f016f531515c0238d0e0cf74d0123a2bf665aeb
Marcelo Buongermino <marcelo.buongermino@fcamara.com>
2024-06-26 16:00:54 -0300

    improved swagger doc and a few improvements

--- GitHubApiConnector/Controllers/GitHubConnectorController.cs
+++ GitHubApiConnector/Controllers/GitHubConnectorController.cs
@@ -1,15 +1,18 @@
-﻿using GitHubApiConnector.Domain.Entities;
-using GitHubApiConnector.UseCases;
+﻿using GitHubApiConnector.UseCases;
+using GitHubApiConnector.UseCases.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace GitHubApiConnector.Api.Controllers;
 
-[Route("api/[controller]/repositories")]
+[Produces("application/json")]
+[Route("api/repositories")]
 [ApiController]
 public class GitHubConnectorController : ControllerBase
 {
     [HttpGet("fetch-and-save")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [SwaggerOperation(Summary = "Fetch github API and stores featured repositories", Description = "Fetch github API and stores featured repositories of some chosen languages")]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "Returns no content")]
     public async Task<IActionResult> FetchAndSaveRepositories([FromServices] IFetchAndSaveRepositoriesUseCase fetchAndSaveRepositoriesUseCase)
     {
         await fetchAndSaveRepositoriesUseCase.Execute();
@@ -18,19 +21,26 @@ public class GitHubConnectorController : ControllerBase
     }
 
     [HttpGet("all")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<GitHubRepo>))]
+    [SwaggerOperation(Summary = "Returns a list of repositories", 
+                        Description = "Returns a list of repositories, per pagination (deafult value is 10) and can be used a language as filter. If the specified language does not exists, no repos will be listed")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Returns a list of repositories, per pagination (deafult value is 10)", typeof(List<GitHubRepositoryDTO>))]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "Returns no content if there is no repository in database")]
     public async Task<IActionResult> ListRepositories([FromServices] IGetAllGitHubRepositoriesUseCase getAllGitHubRepositoriesUseCase,
                                                         string filterByLanguage = "", int pageNumber = 1, int pageSize = 10)
     {
         var repositories = await getAllGitHubRepositoriesUseCase.Execute(filterByLanguage, pageNumber, pageSize);
 
+        if (repositories.Count == 0) 
+            return NoContent();
+
         return Ok(repositories);
     }
 
     [HttpGet("{id}")]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GitHubRepo))]
+    [SwaggerOperation(Summary = "Get a repository details by ID", Description = "Returns the requested repository details by Id")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Returns the request repository", typeof(GitHubRepositoryDetailsDTO))]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "Returns no content if the repository could not be found")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "If the Id is invalid")]
     public async Task<IActionResult> ListRepositoryById([FromServices] IGetGitHubRepositoryByIdUseCase getGitHubRepositoryByIdUseCase, Guid id)
     {
         if (id == Guid.Empty) return BadRequest();
